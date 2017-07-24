3f6ec740aebb0809360545b37b96ff8f6525e113
Janak <janakrule@gmail.com>
2017-07-24 22:07:52 +0530

    first commit

---
+++ OOPS EXAMPLE/1.php
@@ -0,0 +1,14 @@
+<?php
+class abc
+{
+ //function with no argument & no return value
+ function show()
+ {
+  echo "Welcome in my class().";
+ }
+}
+$obj=new abc();
+$obj->show();
+//abc::show();
+
+?>
